4ed843704c5523ce774656cd25c687dc73a18d73
Joseph Engle <josephaengle@gmail.com>
2013-11-12 13:31:24 -0600

    updated user
    updated the username.
    This tool will be for the Digital Resource Center if and when  I figure
    out what APC is.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require('libs/GimmeMe.php');
 
 // config
 $config = array();
-$config['gb_username']    = 'funkatron'; // fill in your gimme bar username
+$config['gb_username']    = 'joe_engle'; // fill in your gimme bar username
 $config['gb_addthis_pubid']    = null; // fill in your addthis pubid
 $config['gb_collection']  = null; // not supported ATM
 $config['gb_cache_ttl']   = 15*60; // 15 minutes
@@ -17,3 +17,4 @@ $config['templates']      = $templates; // array, comes from templates/templates
 // Now actually create the GimmeMe class and display
 $gm = new GimmeMe($config);
 $gm->go();
+?>
